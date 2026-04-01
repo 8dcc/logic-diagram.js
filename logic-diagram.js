58799094928ba8cebffdf1f3745be3823d68f64e
8dcc <8dcc.git@gmail.com>
2026-04-01 21:02:29 +0200

    Add GPLv3 license comment

--- logic-diagram.js
+++ logic-diagram.js
@@ -1,4 +1,25 @@
-/* logic-diagram.js — interactive logic gate diagrams */
+/*
+ * Copyright 2026 8dcc
+ *
+ * This file is part of logic-gates.js.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * For more information, see: https://github.com/8dcc/logic-gates.js
+ */
+
 (function(global) {
 'use strict';
 
