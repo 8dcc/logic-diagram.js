140baa424ad21ba0a2ee206090736b834a69da0a
8dcc <8dcc.git@gmail.com>
2026-04-07 18:48:53 +0200

    Draw diagonal lines relative to reserved space for destination stage

--- logic-diagram.js
+++ logic-diagram.js
@@ -787,9 +787,8 @@ function renderWires(graph, layout, simState) {
             if (sx < tx - 5) {
                 /* Forward wire: horizontal to dst_stage-0.5, diagonal to
                  * dst_stage-0.25, then short stub to pin. */
-                const stageCx = tx + GATE_DEFS[node.type].halfWidth;
-                const diagX0  = stageCx - 0.5 * COL_SPACING;
-                const diagX1  = stageCx - 0.35 * COL_SPACING;
+                const diagX0  = dstPos.x - 0.5 * COL_SPACING;
+                const diagX1  = dstPos.x - 0.35 * COL_SPACING;
                 d = `M${sx},${sy} H${diagX0} L${diagX1},${ty} H${tx}`;
             } else {
                 const exitX = srcPos.x + 0.25 * COL_SPACING;
